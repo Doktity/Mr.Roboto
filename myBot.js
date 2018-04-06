b78d68aea0bfd56428369cca4891e7a89271d753
Doktity <38131269+Doktity@users.noreply.github.com>
2018-04-06 21:44:01 +0200

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -2,26 +2,31 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const nb_image = 5;
 
+/*Fonction Aléatoire pour avoir des images random */
 function alea(){
 	return Math.floor(Math.random() * nb_image);
 }
 
+/* Quand la console est prête */
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
+/* Le ping pong */
 client.on("message", (message) => {
   if (message.content.startsWith("$ping")) {
     message.channel.send("pong!");
   }
 });
 
+/* Le gateau */
 client.on("message", (message) => {
 	if (message.content.startsWith("$Gateau") || message.content.startsWith("$gateau")){
 		message.channel.send("au chocolat");
 	}
 });
 
+/* Images aléatoires de Watanabe You */
 client.on("message", (message) => {
 	if (message.content.startsWith("$you")) {
 		message.channel.send("", {
@@ -30,6 +35,7 @@ client.on("message", (message) => {
 	}
 });
 
+/* Quand quelqu'un est nul */
 client.on("message", (message) => {
 	if (message.content.startsWith("$nul")) {
 		message.channel.send("gros naze", {
@@ -38,6 +44,7 @@ client.on("message", (message) => {
 	}
 });
 
+/* Images aléatoires quand on est content */
 client.on("message", (message) => {
 	if (message.content.startsWith("$joie")) {
 		message.channel.send("La joie", {
@@ -46,12 +53,14 @@ client.on("message", (message) => {
 	}
 });
 
+/* Les 2000 */
 client.on("message", (message) => {
 	if (message.content.startsWith("$2000")){
 		message.channel.send("LES 2000 SONT DEBILES");
 	}
 });
 
+/* Les 1999 */
 client.on("message", (message) => {
 	if (message.content.startsWith("$1999")) {
 		message.channel.send("Les 1999, ces dieux :heart:", {
@@ -60,4 +69,5 @@ client.on("message", (message) => {
 	}
 });
 
+/* Le login pour se connecter avec le robot */
 client.login(process.env.BOT_TOKEN);
