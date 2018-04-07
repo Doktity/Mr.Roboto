69f7e27a979d5e4db53cd3c26b35e1245f619f96
SithMann <38142935+SithMann@users.noreply.github.com>
2018-04-07 17:56:47 +0200

    Delete testFichiers.js

--- testFichiers.js
+++
@@ -1,6 +0,0 @@
-/* Eric Tan */
-client.on("message", (message) => {
-	if (message.content.startsWith("$Tan") || message.content.startsWith("$tan")){
-		message.channel.send("HENRI TAN !! ");
-	}
-});
