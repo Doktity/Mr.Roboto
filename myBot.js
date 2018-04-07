2b9238f85b1fba488041dbd27ab4ea843422fb0e
Doktity <38131269+Doktity@users.noreply.github.com>
2018-04-08 01:37:35 +0200

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");		//Pour utiliser la librairie discord.js
+
 const client = new Discord.Client();		//Pour créer un nouveau client discord
 const nb_image = 5;				// Nombre d'images pour le générateur de valeur aléatoire
-const ytdl = require('ytdl-core');
 
 /*Fonction Aléatoire pour avoir des images random */
 function alea(){
