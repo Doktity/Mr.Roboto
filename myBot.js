626f960c3c20e44536864dd7953cf7e9b923c082
Doktity <38131269+Doktity@users.noreply.github.com>
2018-04-08 00:43:03 +0200

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -117,7 +117,7 @@ client.on('message', message => {
 });
 
 
-client.on('message', message => {
+/*client.on('message', message => {
 	if (message.content.startWith('$play')) {
 		// Note that this will only work if the message was sent in a guild
 		// and the author is actually in a voice channel.
@@ -127,7 +127,7 @@ client.on('message', message => {
 		channel.join()
 		message.channel.send("coucou");
 	}
-});
+});*/
 
 /* Le login pour se connecter avec le robot */
 client.login(process.env.BOT_TOKEN);
