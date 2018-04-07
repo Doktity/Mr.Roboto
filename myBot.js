7d3025e79490a8dac7a6a80f8e5d5d6a4ae6f33c
SithMann <38142935+SithMann@users.noreply.github.com>
2018-04-07 20:58:22 +0200

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -85,5 +85,38 @@ client.on("message", (message) => {
 	}
 });
 
+/* Interaction dans un channel audio */
+bot.on('message', message => {
+
+  if (message.content.startsWith('!play')) {
+    // On récupère le premier channel audio du serveur
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice' })
+      .first()
+    // On récupère les arguments de la commande 
+    // il faudrait utiliser une expression régulière pour valider le lien youtube
+    let args = message.content.split(' ')
+    // On rejoint le channel audio
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        // On démarre un stream à partir de la vidéo youtube
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+          connection.disconnect()
+        })
+        // On envoie le stream au channel audio
+        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
+        connection
+          .playStream(stream)
+          .on('end', function () {
+            connection.disconnect()
+          })
+      })
+  }
+
+})
+
 /* Le login pour se connecter avec le robot */
 client.login(process.env.BOT_TOKEN);
