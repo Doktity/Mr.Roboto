58a1d6c85d6f4677022890c8cb197eb51488a256
Doktity <38131269+Doktity@users.noreply.github.com>
2018-04-07 23:31:20 +0200

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -91,27 +91,28 @@ client.on('message', message => {
   if (message.content.startsWith('$faitpeterleson')) {
     // On récupère le premier channel audio du serveur
     let voiceChannel = message.member.channels;
-    //  .filter(function (channel) { return channel.type === 'voice' })
-      //.first()
+      .filter(function (channel) { return channel.type === 'voice' });
+      .first();
     // On récupère les arguments de la commande 
     // il faudrait utiliser une expression régulière pour valider le lien youtube
-   // let args = message.content.split('http://www.youtube.com/*')
+    let args = message.content.split('http://www.youtube.com/*');
     // On rejoint le channel audio
-    voiceChannel.join()
-      .then(connection => console.log ('connection') {
+    voiceChannel
+      .join();
+      .then(function (connection) {
         // On démarre un stream à partir de la vidéo youtube
-       /* let stream = YoutubeStream(args[1])
+        let stream = YoutubeStream(args[1]);
         stream.on('error', function () {
-          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-          connection.disconnect()
-        })
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(");
+          connection.disconnect();
+        });
         // On envoie le stream au channel audio
         // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
         connection
-          .playStream(stream)
+          .playStream(stream);
           .on('end', function () {
-            connection.disconnect()*/
-          })
+            connection.disconnect();
+          });
       })
   }
 
