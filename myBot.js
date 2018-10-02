4cb15e63c577330da0028470eed243f8255ef92f
Doktity <38131269+Doktity@users.noreply.github.com>
2018-10-02 19:14:47 +0200

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -254,7 +254,8 @@ client.on("message", (message) => {
 
 
 client.on('message', message => {
-	if (message.content.startsWith('coucou') || message.content.startsWith('bonjour') || message.content.startsWith('salut')) {
+	msg = message.content.toLowerCase();
+	if (msg.startsWith('coucou') || msg.startsWith('bonjour') || msg.startsWith('salut')) {
 		let user = message.member.user;
 		message.channel.send('COUCOU ' + user);
 	}
