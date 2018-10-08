e2f0b56c44597ac1994c651ec381055f2f933415
Doktity <38131269+Doktity@users.noreply.github.com>
2018-10-08 21:00:45 +0200

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");		//Pour utiliser la librairie discord.js
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 const sql = require('sqlite');
+sql.open("./scores.sqlite");
 sql.open("./cookie.sqlite");*/
 const client = new Discord.Client();		//Pour créer un nouveau client discord
 const nb_image = 10;				// Nombre d'images pour le générateur de valeur aléatoire
