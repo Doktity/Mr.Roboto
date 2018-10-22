e768e3609452899fc99bc0ec4133f5590d7cf703
SithMann <38142935+SithMann@users.noreply.github.com>
2018-10-22 22:01:53 +0200

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -87,12 +87,7 @@ client.on("message", (message) => {
 		});
 	}
 	
-	/* -18 */
-	if (msg.startsWith(prefixe + "carlos")) {
-		message.channel.send("Ca fait crack crack ici !", {
-			file : "./image/img_crack.png"
-		});
-	}
+
 	
 	// LES IMAGES ALÉATOIRES
 	
