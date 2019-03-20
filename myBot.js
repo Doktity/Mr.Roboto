c7c6efd31163d62a23795fa5c20526bf56945410
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-20 22:00:59 +0100

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -30,7 +30,8 @@ if(!prefixes[message.guild.id]){
 
 client.on("message", (message) => {
 	
-	let video = ["yoshi", "https://www.youtube.com/watch?v=1FHGoAR5Q-c", "cowboy", "https://www.youtube.com/watch?v=yBLdQ1a4-JI"];
+	let video = ["yoshi", "https://www.youtube.com/watch?v=1FHGoAR5Q-c", "cowboy", "https://www.youtube.com/watch?v=yBLdQ1a4-JI",
+		    "evilmorty", "https://www.youtube.com/watch?v=Me6_L5vMWPE"];
 
 	/* Fonction pour lire un fichier audio */
 	function audio(param){
